13a20d417ecab9bc173f688aa298a4d1f98cbd3f
pvn-leo <pavan.leo36dna@gmail.com>
2021-02-18 22:27:33 +0530

    Fixed little warnings

--- input.cpp
+++ input.cpp
@@ -8,5 +8,24 @@ int main()
 	float b;
 	char c;
 	cin>>b>>c;
+	int x; 
+	int y = 7; int z = 8;
+	x = y+z; int final;
+	if(x > y)
+	{
+		if(x > z)
+		{
+			cout<<"x is the greatest";
+			final = x;
+		}
+	}
+	else
+	{
+		if(y > z)
+		{
+			cout<<"y is the greatest";
+			final = y;
+		}
+	}
 	return 0;	
 }
