dcf66720d6af7381fde2d978d105e4a08c07b30c
SamarthGVasist <samarthgvashist2000@gmail.com>
2021-04-25 18:14:16 +0530

    strength reduction optimisation added

--- phase2/Code_optimisation/codeopt.py
+++ phase2/Code_optimisation/codeopt.py
@@ -1,10 +1,67 @@
+import math
+
 f = open("inp.txt","r")
 fout = open("out.txt","w")
 
 list_of_lines = f.readlines()
 dictValues = dict()
 constantFoldedList = []
-print("Quadruple form after Constant Folding")
+
+
+
+
+
+
+
+
+def isPerfectPower(a, b):
+    while a % b == 0:
+        a = a / b
+    if a == 1:
+        return True
+    return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+print("\n")
+print("Strength Reduction ")
+print("--------------------")
+for i in list_of_lines:
+	i = i.strip("\n")
+	op,arg1,arg2,res = i.split()
+	if(arg2=='2' and op=='^'):
+		print(res,"=",arg1,'*',arg1)
+	
+	if(op=='*'):
+		if(arg2=='2'):
+			print(res,"=",arg1,'+',arg1)
+		elif(arg2.isnumeric() and isPerfectPower(int(arg2),2)):
+			print(res,"=",arg1,'<<',int(math.log(int(arg2), 2)))
+				
+	if(op=='/'):
+		
+		if(arg2.isnumeric() and isPerfectPower(int(arg2),2)):
+			print(res,"=",arg1,'>>',int(math.log(int(arg2), 2)))
+		
+
+
+
+
+
+
+'''print("Quadruple form after Constant Folding")
 print("-------------------------------------")
 for i in list_of_lines:
     i = i.strip("\n")
@@ -70,6 +127,21 @@ for i in list_of_lines:
         print(op,arg1,arg2,res)
         constantFoldedList.append([op,arg1,arg2,res])
 
+
+
+
+
+
+
+					
+
+
+
+
+
+
+
+
 print("\n")
 print("Constant folded expression - ")
 print("--------------------")
@@ -87,6 +159,17 @@ for i in constantFoldedList:
             print(i[3],":")
         if(i[0]=="not"):
             print(i[3],"=",i[0],i[1])
+            
+            
+                        
+            
+            
+            
+
+            
+            
+            
+            
 
 print("\n")
 print("After dead code elimination - ")
@@ -104,7 +187,7 @@ for i in constantFoldedList:
         if(i[0]=="label"):
             print(i[3],":")
         if(i[0]=="not"):
-            print(i[3],"=",i[0],i[1])
+            print(i[3],"=",i[0],i[1])'''
                 
         
 
