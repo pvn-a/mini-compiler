5bf28503932fc4fdc63f335d689b9e737ba60967
pvn-leo <pavan.leo36dna@gmail.com>
2021-04-28 11:25:54 +0530

    add inp test files

---
+++ end/test1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+        int b = a*b;
+        if( b < = c ){
+                a = 10;
+                while( a > b ){
+                a = a+1;
+                }
+        }
+        else{
+                a = 20;
+        }
+        a = 100;
+	
+}
---
+++ end/test2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+        int a = 10;
+        int b = 36;
+        while(a < b){
+                cout<<"Hello";
+                if(a > 20){
+                        cout<<"great than 20";
+                        a= a+2;
+                }
+                else{
+                        cout<<"less than 20";
+                        a=a+1;
+                }
+
+        }
+
+	
+}
